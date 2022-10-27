7efbe941bc48d774943331d21f88ff2ed6b3941e
Rajneesh-9088 <rp8735685@gmail.com>
2022-10-28 00:57:41 +0530

    basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import {Container} from "react-bootstrap";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 
 const  App=()=> {
   return (
     < >
-      Rajneesh Shop
+     <Header />
+     <main className="py-3" >
+       <Container>
+       <h1>I Welcome to You</h1>
+       </Container>
+     </main>
+     <Footer />
     </>
   );
 }

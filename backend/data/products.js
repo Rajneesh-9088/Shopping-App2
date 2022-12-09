13584fa434774ca45458e9721ac2adf24658b456
Rajneesh-9088 <rp8735685@gmail.com>
2022-12-10 02:16:07 +0530

    fetching products from backend

--- backend/data/products.js
+++ backend/data/products.js
@@ -19,7 +19,7 @@ const products = [
         brand: "Apple",
         category: 'Electronics',
         price: 599.99,
-        countInStock: 7,
+        countInStock: 7,    
         rating: 4.5,
         numReviews: 4,
     },{
@@ -69,4 +69,4 @@ const products = [
     }
 ]
 
-export default products
+module.exports =  products
